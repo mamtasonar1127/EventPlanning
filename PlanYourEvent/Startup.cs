ae768a4c0926f738b52fa20dbf35f58da755a918
mamtasonar1127 <46754967+mamtasonar1127@users.noreply.github.com>
2019-03-17 15:39:20 -0400

    Add project files.

---
+++ PlanYourEvent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlanYourEvent.Startup))]
+namespace PlanYourEvent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
