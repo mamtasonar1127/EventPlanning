db6aa2bf8d1e7d553ae750c8da3079edac4a53c9
mamtasonar1127 <46754967+mamtasonar1127@users.noreply.github.com>
2019-04-02 13:44:34 -0400

    create unit test for event type

---
+++ PlanYourEvent.Tests/Controllers/EventtypeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//new references
+using System.Web.Mvc;
+using PlanYourEvent.Controllers;
+namespace PlanYourEvent.Tests.Controllers
+{
+    [TestClass]
+    public class EventtypeControllerTest
+    {
+        [TestMethod]
+        public void IndexViewLoads()
+        {
+            //arrange
+
+            //act
+
+            //assert
+        }
+    }
+}
